46c664f4810bb77faf893c951adb69cdeab22028
Wakana722 <wxm7106@mavs.uta.edu>
2026-03-15 23:28:53 -0500

    Implement thread creation for TxWrite operation

--- src/zgt_tm.C
+++ src/zgt_tm.C
@@ -123,6 +123,37 @@ int zgt_tm::TxWrite(long tid, long obno, int thrNum)
   //call the write function (writetx); same as above
 
     // write your code
+
+　　//WMorokoshi [3/15/2026]
+   //again set the txmgr semaphore first. create a thread and first check 
+   // whether this thread can proceed based on the condition variable for that thread.
+   // This is to prevent 2 operations of the same Tx follow one another.
+   // Then get the lock and perform the read operation.
+   //  Call the read function in transaction. 
+   //now create the thread and call the method readtx(void *)
+   
+#ifdef TM_DEBUG
+   printf("\ncreating TxRead thread for Tx: %d\n", tid);fflush(stdout);
+   fflush(stdout);
+#endif
+   pthread_t thread1;
+   
+   struct param *nodeinfo = (struct param*)malloc(sizeof(struct param));
+   nodeinfo->tid = tid;
+   nodeinfo->obno = obno;
+   nodeinfo->Txtype = ' ';
+   nodeinfo->count = --SEQNUM[tid];
+   int status;
+   status = pthread_create(&threadid[thrNum],NULL,readtx,(void*)nodeinfo);
+   if (status){
+     printf("ERROR: return code from pthread_create() is:%d\n", status);
+     exit(-1);
+   }
+   
+#ifdef TM_DEBUG
+   printf("\nexiting TxRead thread create for Tx: %d\n", tid);
+   fflush(stdout);
+#endif
     
    return(0);  // successful operation
  }
